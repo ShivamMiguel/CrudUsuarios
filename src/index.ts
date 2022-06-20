14bae9b785c117ee680d794730358e7bb38c929a
António Miguel <89463747+aantonioMiguel@users.noreply.github.com>
2022-06-20 14:42:40 +0100

    Servidor criado

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello word")
+//Construcão do servidor com express
+import express, {Request,Response,NextFunction}  from 'express';
+
+const app = express();
+
+app.get('/status', (req:Request, res:Response, next:NextFunction) => {
+    res.status(200).send({ foo: 'bar' });
+
+});
+
+app.listen(3000, () => {
+    console.log('Aplicacão executando na porta 3000');
+   
+});
