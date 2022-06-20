67fc9e70464535260fdc6c76cb3af9fe890d2585
António Miguel <89463747+aantonioMiguel@users.noreply.github.com>
2022-06-20 18:05:15 +0100

    adicinando rotas

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
-//Construcão do servidor com express
+//Doc principal da aplicacão
 import express, {Request,Response,NextFunction}  from 'express';
+import userRoute from './routes/users.route';
+import statusRoute from './routes/status.route';
 
 const app = express();
+
+//configuracão da aplicacão.
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Configuracão das routas
+app.use(userRoute);
+app.use(statusRoute);
 
 app.get('/status', (req:Request, res:Response, next:NextFunction) => {
     res.status(200).send({ foo: 'bar' });
 
 });
 
+
+
+// inicializacão do servidor
 app.listen(3000, () => {
     console.log('Aplicacão executando na porta 3000');
    
---
+++ src/routes/users.route.ts
@@ -0,0 +1,44 @@
+
+
+
+
+
+import { Router, Request, Response, NextFunction} from 'express';
+import {StatusCodes} from 'http-status-codes';
+
+
+const userRoute = Router();
+
+userRoute.get ('/users',  (req:Request, res:Response, next:NextFunction) =>{
+    const users =[{userName: 'Renan'}];
+    res.status(StatusCodes.OK).send(users)
+
+});
+
+userRoute.get('/users/:uuid',(req:Request<{uuid:string}>, res:Response, next:NextFunction) => {
+
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({uuid});
+
+});
+
+userRoute.post('/users', (req:Request, res:Response, next:NextFunction) => {
+    const newUser = req.body;
+    console.log(req.body);
+    res.status(StatusCodes.CREATED).send(newUser);
+})
+
+userRoute.put('/users/:uuid',(req:Request<{uuid:string}>, res:Response, next:NextFunction) => {
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({uuid});
+
+
+
+});
+
+userRoute.delete('/users/:uuid', (req:Request, res:Response, NextFunction) => {
+    const uuid = req.params.uuid;
+    res.sendStatus(StatusCodes.OK);
+})
+
+export default userRoute;
